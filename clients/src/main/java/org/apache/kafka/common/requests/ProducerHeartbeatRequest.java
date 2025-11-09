8e7a14495df9c0bb2f21cda559d1391ae58dd65b
TaiJu Wu <tjwu1217@gmail.com>
2025-11-09 15:53:55 +0800

    add json and  protocol

---
+++ clients/src/main/java/org/apache/kafka/common/requests/ProducerHeartbeatRequest.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.kafka.common.requests;
+
+import org.apache.kafka.common.message.ProducerHeartbeatRequestData;
+import org.apache.kafka.common.message.ProducerHeartbeatResponseData;
+import org.apache.kafka.common.protocol.ApiKeys;
+import org.apache.kafka.common.protocol.Errors;
+import org.apache.kafka.common.protocol.Readable;
+
+public class ProducerHeartbeatRequest extends AbstractRequest {
+    public static class Builder extends AbstractRequest.Builder<ProducerHeartbeatRequest> {
+        private final ProducerHeartbeatRequestData data;
+
+        public Builder(ProducerHeartbeatRequestData data) {
+            super(ApiKeys.PRODUCER_HEARTBEAT);
+            this.data = data;
+        }
+
+        @Override
+        public ProducerHeartbeatRequest build(short version) {
+            return new ProducerHeartbeatRequest(data, version);
+        }
+
+        @Override
+        public String toString() {
+            return data.toString();
+        }
+    }
+
+    private final ProducerHeartbeatRequestData data;
+
+    private ProducerHeartbeatRequest(ProducerHeartbeatRequestData data, short version) {
+        super(ApiKeys.PRODUCER_HEARTBEAT, version);
+        this.data = data;
+    }
+
+    @Override
+    public ProducerHeartbeatRequestData data() {
+        return data;
+    }
+
+    @Override
+    public ProducerHeartbeatResponse getErrorResponse(int throttleTimeMs, Throwable e) {
+        Errors error = Errors.forException(e);
+        return new ProducerHeartbeatResponse(new ProducerHeartbeatResponseData()
+                .setThrottleTimeMs(throttleTimeMs)
+                .setErrorCode(error.code()));
+    }
+
+    public static ProducerHeartbeatRequest parse(Readable readable, short version) {
+        return new ProducerHeartbeatRequest(new ProducerHeartbeatRequestData(readable, version),
+                version);
+    }
+
+}
---
+++ clients/src/main/java/org/apache/kafka/common/requests/ProducerHeartbeatResponse.java
@@ -0,0 +1,73 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.kafka.common.requests;
+
+import org.apache.kafka.common.message.ProducerHeartbeatResponseData;
+import org.apache.kafka.common.protocol.ApiKeys;
+import org.apache.kafka.common.protocol.Errors;
+import org.apache.kafka.common.protocol.Readable;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+public class ProducerHeartbeatResponse extends AbstractResponse {
+    private final ProducerHeartbeatResponseData data;
+
+    public ProducerHeartbeatResponse(ProducerHeartbeatResponseData data) {
+        super(ApiKeys.PRODUCER_HEARTBEAT);
+        this.data = data;
+    }
+
+    @Override
+    public ProducerHeartbeatResponseData data() {
+        return data;
+    }
+
+    @Override
+    public int throttleTimeMs() {
+        return data.throttleTimeMs();
+    }
+
+    @Override
+    public void maybeSetThrottleTimeMs(int throttleTimeMs) {
+        data.setThrottleTimeMs(throttleTimeMs);
+    }
+
+    @Override
+    public Map<Errors, Integer> errorCounts() {
+        Map<Errors, Integer> errorCounts = new EnumMap<>(Errors.class);
+        errorCounts.put(Errors.forCode(data.errorCode()), 1);
+        return errorCounts;
+    }
+
+    public short errorCode() {
+        return this.data.errorCode();
+    }
+
+    public String errorMessage() {
+        return this.data.errorMessage();
+    }
+
+    public static ProducerHeartbeatResponse parse(Readable readable, short version) {
+        return new ProducerHeartbeatResponse(new ProducerHeartbeatResponseData(readable, version));
+    }
+
+    @Override
+    public boolean shouldClientThrottle(short version) {
+        return true;
+    }
+}
